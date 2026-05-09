d00ce7e0290eedf9562eadc60ccdb2a23568c24f
finlysta-dev <finlysta@gmail.com>
2026-05-09 18:12:32 +0530

    ff

--- app/internships/[slug]/page.tsx
+++ app/internships/[slug]/page.tsx
@@ -7,7 +7,7 @@ import {
   MapPin, Clock, Building2, Calendar,
   CheckCircle, Bookmark, Share2, Zap,
   ExternalLink, Award, GraduationCap,
-  Briefcase, Target, Sparkles, ChevronRight, TrendingUp, Heart, AlertCircle
+  Briefcase, Target, Sparkles, ChevronRight, ChevronUp, TrendingUp, Heart, AlertCircle
 } from "lucide-react";
 
 interface Internship {
